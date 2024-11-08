7e05e34a9ce3106c29fb5c7e20a0c0b4c1553569
Brian Dobbs <bdobbs@sketchdev.io>
2024-11-08 10:54:07 -0600

    get starbot to display on map

---
+++ src/components/Character.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Character: React.FC = () => {
+  return <div className="character_spritesheet pixel-art"></div>;
+};
+
+export default Character;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Map: React.FC = () => {
-  return <div className="map pixel-art"></div>;
+  return (
+    <div className="map pixel-art">
+      <div className="character">
+        <div className="character_spritesheet pixel-art"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
